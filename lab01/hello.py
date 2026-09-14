b99282c237b16822d413ab21b8352ffcf845242d
Ciara Murphy <ciara.murphy561@gmail.com>
2026-09-14 15:04:04 +0000

    Complete Lab 1

---
+++ lab01/hello.py
@@ -0,0 +1,7 @@
+name =input("What is your name?")
+print("Hello," + name + "!")
+course =input("What are you studying?")
+print("You are studying" + course + "!")
+interest = input("What area interests you?")
+print("You're interested in" + interest +"!")
+
